f58402bd3aaa6eddb37c32ed89585b5e09758215
hillyc12 <Mayer.H.Cohen@gmail.com>
2021-11-17 16:52:05 -0500

    Added Hebrew Date brought in luxon - use unformated dates from api don't need to localize and add todays date

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Tephilos from "./Components/Tephilos/Tephilos";
 import Card from "./Components/UI/Card";
 import ZemanimForDay from "./Components/Zemanim/ZemanimForDay";
+import { DateTime } from "luxon";
 const dispDate = new Date();
-
+const dtHebrew = DateTime.now().reconfigure({ outputCalendar: "hebrew" });
+//dtHebrew.outputCalendar; //=> 'hebrew'
+//dtHebrew.toLocaleString();
 function App() {
   return (
     <Card>
       <h1 className="App-header"> Zemanin Kol Shimshon </h1>
+      <h2 className="App-header2"> {dtHebrew.toLocaleString()} </h2>
       <Tephilos date={dispDate} />
       <ZemanimForDay />
     </Card>
--- src/TimeFunctions/GetSunriseSunset.js
+++ src/TimeFunctions/GetSunriseSunset.js
@@ -1,6 +1,6 @@
 const zemanimData = async () => {
   const response = await fetch(
-    "https://api.sunrise-sunset.org/json?lat=40.10256&lng=-74.209786&date=2021-11-15"
+    "https://api.sunrise-sunset.org/json?lat=40.10256&lng=-74.209786&date=today&formatted=0"
   );
   //const td = new Date();
   //const tomorrow = new Date(td.setDate(td.getDate() + 1));
@@ -16,30 +16,11 @@ const zemanimData = async () => {
   //const zemaniim2Json = await response2.json();
   const sunrise = zemaniimJson.results.sunrise;
   const sunset = zemaniimJson.results.sunset;
-  /* console.log(`Resonse1: ${zemaniimJson.results.sunrise}`);
-  console.log(`Resonse2: ${zemaniim2Json.results.sunrise}`); */
-  const [hour, min, scnd] = sunrise.split(":");
-  const [second] = scnd.split(" ");
-  const tod = new Date();
-  const month = tod.getMonth();
-  const year = tod.getFullYear();
-  const day = tod.getDate();
-  const sd = new Date(year, month, day, hour, min, second);
-
-  const sunriseLocal = new Date(sd.setHours(sd.getHours() - 5));
+  const sd = new Date(sunrise);
+  const sunriseLocal = new Date(sunrise);
   const alosLocal = new Date(sd.setMinutes(sd.getMinutes() - 72));
+  const sunsetLocal = new Date(sunset);
 
-  const [shour, smin, sscnd] = sunset.split(":");
-  const shour12 = Number(shour) + 12;
-
-  const [ssecond] = sscnd.split(" ");
-
-  const ss = new Date(year, month, day, shour12, smin, ssecond);
-  const sunsetLocal = new Date(ss.setHours(ss.getHours() - 5));
-
-  console.log(`Netz: ${sunriseLocal}`);
-  console.log(`Alos: ${alosLocal}`);
-  console.log(`Shkiah: ${sunsetLocal}`);
   const sunriseStr = ExtractTime(sunriseLocal);
   const timeInDay = sunsetLocal - sunriseLocal;
   const halfDay = Number(timeInDay) / 2;
@@ -48,9 +29,8 @@ const zemanimData = async () => {
       sunriseLocal.getUTCMilliseconds() + Number(halfDay)
     )
   );
-
+  const tod = sunriseLocal.getDate();
   const alosStrg = ExtractTime(alosLocal);
-
   const sunsetStr = ExtractTime(sunsetLocal);
   const chattzosStr = ExtractTime(chattzos);
   const zemanimForDay = {
@@ -60,8 +40,6 @@ const zemanimData = async () => {
     midday: chattzosStr,
     sunset: sunsetStr,
   };
-  console.log(`chatzos: ${chattzos}`);
-
   return zemanimForDay;
 };
 
